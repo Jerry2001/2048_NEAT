5dfbe5fad0914bdccfadf932ace396117d5d1ea7
Jerry2001 <ngohongphuc2001@gmail.com>
2021-05-03 10:59:53 -0500

    Add NEAT lib, add config, run config file in main py

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import neat
+import game.tk_gui as gui
+import numpy as np
+
+from game import utils
+from game.utils import Direction
+from game.core_2048 import GameCore
+from game.utils import State
+
+game = GameCore(4)
+GUI = gui.GameGUI(game)
+
+board = (np.array(game.Board())).flatten()
+
+print(board)
+
+def run(config_file):
+    # Load configuration.
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+
+    # Create the population, which is the top-level object for a NEAT run.
+    p = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(5))
+
+    # Run for up to 300 generations.
+    winner = p.run(eval_genomes, 300)
+
+    # Display the winning genome.
+    print('\nBest genome:\n{!s}'.format(winner))
