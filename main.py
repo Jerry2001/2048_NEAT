8d839db2b31b7960a507ce16ff595b23a5b41da2
Jerry2001 <ngohongphuc2001@gmail.com>
2021-05-03 11:34:37 -0500

    First working version of game

--- main.py
+++ main.py
@@ -10,10 +10,58 @@ from game.utils import State
 
 game = GameCore(4)
 GUI = gui.GameGUI(game)
+NOT_MOVED_RESTART_THRESHOLD = 10
 
-board = (np.array(game.Board())).flatten()
+def map_neuron_to_move(pos):
+	if pos == 0:
+		return Direction.UP
+	elif pos == 1:
+		return Direction.DOWN
+	elif pos == 2:
+		return Direction.LEFT
+	elif pos == 3:
+		return Direction.RIGHT
 
-print(board)
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        eval_genome(genome_id, genome, config)
+
+def eval_genome(genome_id, genome, config):
+	genome.fitness = 0.0
+	net = neat.nn.FeedForwardNetwork.create(genome, config)
+	game.restart_game()
+	GUI.set_game(game)
+	game_over = False
+	board = game.Board()
+	consecutive_not_moved = 0
+	successful_moves = 0
+
+	while not game_over:
+		in_neurons = (np.array(game.Board())).flatten()
+		output = net.activate(in_neurons)
+    	# Use the 'most activated' output neuron as the intended direction
+        # Generate list of tuples which are (direction, output weight)
+		output_moves = [(map_neuron_to_move(i), output[i]) for i in range(len(output))]
+		output_moves = sorted(output_moves, key=lambda x: x[1])
+
+        # Try move the board starting with the highest weighted output direction
+		for (direction, weight) in output_moves:
+			moved = game.try_move(direction)
+			if moved:
+				break
+
+		if moved:
+			GUI.repaint_board()
+			successful_moves = successful_moves + 1
+		else:
+			consecutive_not_moved = consecutive_not_moved + 1
+
+		if game.State() == State.WIN or game.State() == State.LOSS:
+			game_over = True
+		elif consecutive_not_moved == NOT_MOVED_RESTART_THRESHOLD:
+			game_over = True
+	genome.fitness = game.Score()
+
 
 def run(config_file):
     # Load configuration.
@@ -35,3 +83,11 @@ def run(config_file):
 
     # Display the winning genome.
     print('\nBest genome:\n{!s}'.format(winner))
+
+if __name__ == '__main__':
+    # Determine path to configuration file. This path manipulation is
+    # here so that the script will run successfully regardless of the
+    # current working directory.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.py')
+    run(config_path)
