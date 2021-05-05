bc897eaa396a38bd1e43810d3287f67cd46af039
Jerry2001 <ngohongphuc2001@gmail.com>
2021-05-05 04:02:34 -0500

    Add hyperneat xor and its config, add working version of hyperneat 2048

--- main_hyperneat.py
+++ main_hyperneat.py
@@ -30,6 +30,96 @@ NOT_MOVED_RESTART_THRESHOLD = 20
 #for i in range(2, -3, -1):
 #	for j in range(-1, 3):
 
-game.restart_game()
-board = game.Board()
+def map_neuron_to_move(pos):
+	if pos == 0:
+		return Direction.UP
+	elif pos == 1:
+		return Direction.DOWN
+	elif pos == 2:
+		return Direction.LEFT
+	elif pos == 3:
+		return Direction.RIGHT
 
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        eval_genome(genome_id, genome, config)
+
+def eval_genome(genome_id, genome, config):
+	genome.fitness = 0.0
+	cppn = neat.nn.FeedForwardNetwork.create(genome, config)
+	net = create_phenotype_network(cppn, sub)
+
+	#for i in range(0, 5):
+	game.restart_game()
+	GUI.set_game(game)
+	game_over = False
+	board = game.Board()
+	consecutive_not_moved = 0
+	successful_moves = 0
+
+	while not game_over:
+		in_neurons = (np.array(game.Board())).flatten()
+		output = net.activate(in_neurons)
+    	# Use the 'most activated' output neuron as the intended direction
+        # Generate list of tuples which are (direction, output weight)
+		output_moves = [(map_neuron_to_move(i), output[i]) for i in range(len(output))]
+		output_moves = sorted(output_moves, key=lambda x: x[1])
+
+        # Try move the board starting with the highest weighted output direction
+		for (direction, weight) in output_moves:
+			moved = game.try_move(direction)
+			if moved:
+				break
+
+		if moved:
+			GUI.repaint_board()
+			successful_moves = successful_moves + 1
+		else:
+			consecutive_not_moved = consecutive_not_moved + 1
+
+		if game.State() == State.WIN or game.State() == State.LOSS:
+			game_over = True
+		elif consecutive_not_moved == NOT_MOVED_RESTART_THRESHOLD:
+			game_over = True
+	genome.fitness = game.Score() 
+	#genome.fitness /= 5.0
+
+# Create the population and run the XOR task by providing the above fitness function.
+def run(config_file):
+	config = neat.config.Config(neat.genome.DefaultGenome, neat.reproduction.DefaultReproduction,
+                            neat.species.DefaultSpeciesSet, neat.stagnation.DefaultStagnation,
+                            config_path)
+	p = neat.population.Population(config)
+	stats = neat.statistics.StatisticsReporter()
+	p.add_reporter(stats)
+	p.add_reporter(neat.reporting.StdOutReporter(True))
+
+	winner = p.run(eval_genomes, 20)
+
+    # Display the winning genome.
+	print('\nBest genome:\n{!s}'.format(winner))
+	visualize.plot_stats(stats, ylog=False, view=True)
+	visualize.plot_species(stats, view=True)
+
+# If run as script.
+if __name__ == '__main__':
+	local_dir = os.path.dirname(__file__)
+	config_path = os.path.join(local_dir, 'config_cppn')
+	run(config_path)
+	
+    # Verify network output against training data.
+    # print('\nOutput:')
+    # cppn = neat.nn.FeedForwardNetwork.create(winner, config)
+    # winner_net = create_phenotype_network(cppn, sub)
+    # for inputs, expected in zip(xor_inputs, xor_outputs):
+    #     new_input = inputs + (1.0,)
+    #     winner_net.reset()
+    #     for i in range(activations):
+    #         output = winner_net.activate(new_input)
+    #     print("  input {!r}, expected output {!r}, got {!r}".format(inputs, expected, output))
+
+    # Save CPPN if wished reused and draw it to file along with the winner.
+    # with open('hyperneat_xor_cppn.pkl', 'wb') as output:
+    #     pickle.dump(cppn, output, pickle.HIGHEST_PROTOCOL)
+    # draw_net(cppn, filename="hyperneat_xor_cppn")
+    # draw_net(winner_net, filename="hyperneat_xor_winner")
