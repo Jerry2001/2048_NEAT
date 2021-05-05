376f1164604ca3ec7c422563ab87b0bf2e67037e
Jerry2001 <ngohongphuc2001@gmail.com>
2021-05-04 21:33:42 -0500

    Update config, stuck at 4k

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from game.utils import State
 
 game = GameCore(4)
 GUI = gui.GameGUI(game)
-NOT_MOVED_RESTART_THRESHOLD = 10
+NOT_MOVED_RESTART_THRESHOLD = 20
 
 def map_neuron_to_move(pos):
 	if pos == 0:
@@ -30,38 +30,41 @@ def eval_genomes(genomes, config):
 def eval_genome(genome_id, genome, config):
 	genome.fitness = 0.0
 	net = neat.nn.FeedForwardNetwork.create(genome, config)
-	game.restart_game()
-	GUI.set_game(game)
-	game_over = False
-	board = game.Board()
-	consecutive_not_moved = 0
-	successful_moves = 0
+	for i in range(0, 5):
+		game.restart_game()
+		GUI.set_game(game)
+		game_over = False
+		board = game.Board()
+		consecutive_not_moved = 0
+		successful_moves = 0
 
-	while not game_over:
-		in_neurons = (np.array(game.Board())).flatten()
-		output = net.activate(in_neurons)
-    	# Use the 'most activated' output neuron as the intended direction
-        # Generate list of tuples which are (direction, output weight)
-		output_moves = [(map_neuron_to_move(i), output[i]) for i in range(len(output))]
-		output_moves = sorted(output_moves, key=lambda x: x[1])
+		while not game_over:
+			in_neurons = (np.array(game.Board())).flatten()
+			output = net.activate(in_neurons)
+	    	# Use the 'most activated' output neuron as the intended direction
+	        # Generate list of tuples which are (direction, output weight)
+			output_moves = [(map_neuron_to_move(i), output[i]) for i in range(len(output))]
+			output_moves = sorted(output_moves, key=lambda x: x[1])
 
-        # Try move the board starting with the highest weighted output direction
-		for (direction, weight) in output_moves:
-			moved = game.try_move(direction)
+	        # Try move the board starting with the highest weighted output direction
+			for (direction, weight) in output_moves:
+				moved = game.try_move(direction)
+				if moved:
+					break
+
 			if moved:
-				break
+				GUI.repaint_board()
+				successful_moves = successful_moves + 1
+			else:
+				consecutive_not_moved = consecutive_not_moved + 1
 
-		if moved:
-			GUI.repaint_board()
-			successful_moves = successful_moves + 1
-		else:
-			consecutive_not_moved = consecutive_not_moved + 1
-
-		if game.State() == State.WIN or game.State() == State.LOSS:
-			game_over = True
-		elif consecutive_not_moved == NOT_MOVED_RESTART_THRESHOLD:
-			game_over = True
-	genome.fitness = game.Score()
+			if game.State() == State.WIN or game.State() == State.LOSS:
+				game_over = True
+			elif consecutive_not_moved == NOT_MOVED_RESTART_THRESHOLD:
+				game_over = True
+		genome.fitness += game.Score() 
+	genome.fitness /= 5.0
+	#game.Score() + game.Max_tile()*5 - game.Move()
 
 
 def run(config_file):
@@ -80,7 +83,7 @@ def run(config_file):
     p.add_reporter(neat.Checkpointer(5))
 
     # Run for up to 300 generations.
-    winner = p.run(eval_genomes, 15)
+    winner = p.run(eval_genomes, 20)
 
     # Display the winning genome.
     print('\nBest genome:\n{!s}'.format(winner))
